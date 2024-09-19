2fa3975f1f4fd51c553a1d22d79dda81d45c8da0
tetronitte <68910823+tetronitte@users.noreply.github.com>
2024-09-19 22:46:15 +0200

    refactoring et optimisation du code

--- src/main/java/Algorithme.java
+++ src/main/java/Algorithme.java
@@ -1,17 +1,81 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.text.Normalizer;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Algorithme {
 
-    private static final int SUPERIEUR_A = 10;
-    private static final int ITERATION_MAX = 50;
-    
-    public static int calcul(List<Integer> list, int iteration) {
+    private static final int BETTER_THAN = 10;
+    private static final int MAX_ITERATION = 50;
+    private static final String NAME1_OUT = "Entrez le premier nom : ";
+    private static final String NAME2_OUT = "Entrez le deuxième nom : ";
+
+    private Scanner scanner = new Scanner(System.in);
+    private String nom1;
+    private String nom2;
+    private String concatenatedNames;
+    private Map<Character, Integer> alphabet = new HashMap<>();
+    private Map<String, String> compatibilities = new LinkedHashMap<>();
+
+    public Algorithme() {
+        initAlphabet();
+        initCompatibilities();
+    }
+
+    /**
+     * Méthode principale
+     */
+    public void execute() {
+
+        readNames();
+        calculOccurence();
+
+        displayCompatibilities();
+
+        scanner.close();
+    }
+
+    /**
+     * Initialise une map des compatibilités
+     */
+    public void initCompatibilities() {
+        compatibilities.put("AMOUR", "amoureuse");
+        compatibilities.put("AMIS", "amicale");
+        compatibilities.put("SEXE", "sexuelle");
+        compatibilities.put("COOK", "culinaire");
+        compatibilities.put("SPORT", "sportive");
+        compatibilities.put("MUSIC", "musicale");
+        compatibilities.put("ASTRO", "astrologique");
+        compatibilities.put("POLITIQUE", "politique");
+    }
+
+    /**
+     * Lit les noms dans la console et les concatènes
+     */
+    private void readNames() {
+        // Lecture du premier nom
+        System.out.print(NAME1_OUT);
+        nom1 = removeAccent(scanner.nextLine()).toUpperCase();
+
+        // Lecture du deuxième nom
+        System.out.print(NAME2_OUT);
+        nom2 = removeAccent(scanner.nextLine()).toUpperCase();
+
+        // Concaténation des deux noms
+        concatenatedNames = nom1 + nom2;
+    }
+
+    /**
+     * Effectue le calcul de compatibilité
+     * @param list
+     * @param iteration
+     * @return le pourcentage de compatibilité
+     */
+    private int calcul(List<Integer> list, int iteration) {
 
         int resultat = 0;
         List<Integer> tmp = new ArrayList<>();
         for (int i = 0; i < list.size() - 1; i++) {
-            if (list.get(i) + list.get(i + 1) < SUPERIEUR_A) {
+            if (list.get(i) + list.get(i + 1) < BETTER_THAN) {
                 tmp.add(list.get(i) + list.get(i + 1));
             } else {
                 int x = list.get(i) + list.get(i + 1);
@@ -20,7 +84,7 @@ public class Algorithme {
             }
         }
         if (tmp.size() != 2) {
-            if (iteration < ITERATION_MAX) {
+            if (iteration < MAX_ITERATION) {
                 resultat = calcul(tmp, iteration + 1);
             } else {
                 resultat = tmp.get(0) * 10 + tmp.get(1);
@@ -29,5 +93,61 @@ public class Algorithme {
             resultat = tmp.get(0) * 10 + tmp.get(1);
         }
         return resultat;
+    }
+
+    /**
+     * Affiche les différentes compatibilités
+     */
+    private void displayCompatibilities() {
+        int percent;
+        List<Integer> list;
+        for (Map.Entry<String, String> compatibility : compatibilities.entrySet()) {
+            list = genererListe(compatibility.getKey());
+            percent = calcul(list, 0);
+            System.out.println("Compatibilite "+compatibility.getValue()+" : "+percent+"%");
+        }
+    }
+
+    /**
+     * Calcul le nombre d'occurences des lettres dans les noms concaténés
+     */
+    private void calculOccurence() {
+        for (char lettre : concatenatedNames.toCharArray()) {
+            if (alphabet.containsKey(lettre)) {
+                alphabet.put(lettre, alphabet.get(lettre) + 1);
+            }
+        }
+    }
+
+    /**
+     * Initialise une map avec l'alphabet
+     */
+    private void initAlphabet() {
+        for (int i = 65 ; i < 91 ; i++) {
+            alphabet.put(Character.toString(i).charAt(0), 0);
+        }
+    }
+
+    /**
+     * Permet de supprimer tous les accents
+     * @param nom
+     * @return chaine sans accents
+     */
+    private String removeAccent(String nom) {
+        // Suppression des accents
+        return Normalizer.normalize(nom, Normalizer.Form.NFD)
+                .replaceAll("\\p{InCombiningDiacriticalMarks}+", "");
+    }
+
+    /**
+     * Parse les caractères de la compatibilité et insère dans une liste le nombre d'occurences
+     * @param compatibility
+     * @return liste d'occurences
+     */
+    private List<Integer> genererListe(String compatibility) {
+        return compatibility.chars()
+            .filter(letter -> alphabet.containsKey((char) letter))
+            .mapToObj(occurence -> alphabet.get((char)occurence))
+            .collect(Collectors.toList());
     }
 }
--- src/main/java/Choix.java
+++
@@ -1,3 +0,0 @@
-public enum Choix {
-    AMOUR, AMIS, SEXE, COOK
-}
